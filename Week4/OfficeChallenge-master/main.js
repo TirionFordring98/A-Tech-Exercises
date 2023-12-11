137d3c6d6f8a415bdf56fbae7316bd0a20069fc7
Madexr12 <mhmdmonder98@gmail.com>
2023-12-11 18:33:41 +0200

    Porject Upload

--- Week4/OfficeChallenge-master/main.js
+++ Week4/OfficeChallenge-master/main.js
@@ -1,11 +1,63 @@
 class Document {
-    constructor(EmployeeName) {
+    constructor(EmployeeName){
         this.EmployeeName = EmployeeName
     }
 }
-​
-class Employee {
+class Employee{
     constructor(name) {
         this.name = name
+    }
+  work(office) {
+        for (let i = 0; i < 10; i++) {
+            const document = new Document(this.name)
+            office.documents.push(document)
+        }
+    }
+}
+class Manager {
+    constructor(name) {
+        this.name = name
+        this.employees = []
+    }
+    hireEmployee(name) {
+        const newEmployee = new Employee(name)
+        this.employees.push(newEmployee)
+    }
+    askEmployeesToWork(office) {
+        for (let i in this.employees) {
+            this.employees[i].work(office)
+        }
+    }
+}
+class Cleaner {
+    constructor(name) {
+        this.name = name
+    }
+    clean() {
+        console.log("Clean")
+    }
+}
+class Office {
+    constructor() {
+        this.documents = []
+        this.managers = []
+        this.cleaners = []
+    }
+    hireCleaner(name) {
+        const newCleaner = new Cleaner(name)
+        this.cleaners.push(newCleaner)
+    }
+    hireManager(name) {
+        const newManager = new Manager(name)
+        this.managers.push(newManager)
+    }
+    startWorkDay() {
+        for(let c in this.cleaners){
+            this.cleaners[c].clean()
+        }
+        for(let m in this.managers){
+            this.managers[m].askEmployeesToWork(this)
+            console.log('test12');
+        }
     }
 }
